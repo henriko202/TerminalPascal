c5cecde30ecfe4eab8a66bcc3411e3910f0ce429
henriko202 <henriko.foz@gmail.com>
2019-08-27 11:34:01 -0300

    locate, touch, mv, cd, ls implementados

--- README.md
+++ README.md
@@ -3,10 +3,7 @@ Um terminal simples feito em Pascal para a matéria de Linguagens de Programaç�
 
 Comandos:
 
-- ls, cd, mkdir, mv, rmdir, rm, clear, man, locate, cat
+- ls, cd, mkdir, mv, rmdir, rm, clear, man, locate, cat, touch
 
 Implemenatdos:
-- mkdir, rmdir, rm, clear, cat, man(20% feito)
-
-Ainda não implementadaos:
-- ls, cd, mv, locate
+- mkdir, rmdir, rm, clear, cat, touch, ls, cd, mv, man(20% feito)
--- terminal.pas
+++ terminal.pas
@@ -13,16 +13,12 @@ Var
   arg0 : String;
   arg1 : String;
   arg2 : String;
-  tam0 : integer;
   tam1 : integer;
   tam2 : integer;
   charset : TSysCharSet;
   arq: TextFile;
   outputString: String;
-  currLocation : String;
-  Path    : String;
   SR      : TSearchRec;
-  DirList : TStrings;
   texto: Text;
 
 Begin
@@ -31,18 +27,16 @@ Begin
   include(charset, ' ');
 
   entrada := '';
-  GetDir (0,currLocation);
 
   While (CompareText(entrada,'exit')<>0) Do
     Begin
-      write(currLocation);
+      write(GetCurrentDir);
       write('>');
       readLn(entrada);
 
       arg0 := ExtractWord(1,entrada,charset);
       arg1 := ExtractWord(2,entrada,charset);
       arg2 := ExtractWord(3,entrada,charset);
-      tam0 := WordCount(arg0,charset);
       tam1 := WordCount(arg1,charset);
       tam2 := WordCount(arg2,charset);
 
@@ -103,39 +97,75 @@ Begin
       End;
       'ls':
             Begin
-              writeln('NYI!');
+              If FindFirst ('*',faAnyFile And faDirectory,SR)=0 Then
+                Begin
+                  Repeat
+                    With SR Do
+                      Begin
+                        Write(SR.Name + ' ');
+                      End;
+                  Until FindNext(SR)<>0;
+                End;
+              writeln();
+              FindClose(SR);
             End;
       'cd':
             Begin
-              writeln('NYI!');
+              Try
+                ChDir (arg1);
+              Except
+                on E: EInOutError Do Writeln (E.Message);
             End;
-      'mv':
-            Begin
-              writeln('NYI!');
-            End;
-      'locate':
-                Begin
-                  writeln('NYI!');
-                End;
-      'rmdir':
-               Begin
-                 Try
-                   If (tam1 <> 0 ) Then rmdir(arg1);
-                 Except
-                   on E: EInOutError Do writeln(E.Message);
-               End;
     End;
-  'rm' :
-         Begin
-           Try
-             If (tam1 <> 0 ) Then
-               Begin
-                 assign(texto,arg1);
-                 erase(texto);
-               End;
-           Except
-             on E: EInOutError Do writeln(E.Message);
-         End;
+  'mv':
+        Begin
+          If (tam1<>0)Then
+            If (tam2<>0) Then
+              Begin
+                If Not(renamefile(arg1, arg2)) Then writeln('Erro ao mover arquivo!');
+              End;
+        End;
+  'touch':
+           Begin
+             If (tam1<>0) Then filecreate(arg1);
+           End;
+  'locate':
+            Begin
+              If (tam1<>0) Then
+                Begin
+                  If FindFirst ('*'+arg1+'*',faAnyFile And faDirectory,SR)=0 Then
+                    Begin
+                      Repeat
+                        With SR Do
+                          Begin
+                            Write(SR.Name + ' ');
+                          End;
+                      Until FindNext(SR)<>0;
+                    End;
+                  writeln();
+                  FindClose(SR);
+                End;
+              If (tam1=0) Then writeln('Digite o nome do arquivo!');
+            End;
+  'rmdir':
+           Begin
+             Try
+               If (tam1 <> 0 ) Then rmdir(arg1);
+             Except
+               on E: EInOutError Do writeln(E.Message);
+           End;
+End;
+'rm' :
+       Begin
+         Try
+           If (tam1 <> 0 ) Then
+             Begin
+               assign(texto,arg1);
+               erase(texto);
+             End;
+         Except
+           on E: EInOutError Do writeln(E.Message);
+       End;
 End;
 'mkdir':
          Begin
