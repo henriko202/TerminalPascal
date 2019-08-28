ed0bc2fa1302eb74b3dc90cb9e301a8a2fb9c5c1
EricaSaito <eriyuri1213@gmail.com>
2019-08-28 17:11:14 -0300

    Formatação e comentários

--- terminal.pas
+++ terminal.pas
@@ -1,244 +1,236 @@
+//Projeto 1 FILE CONQUEROR _ Explorador de arquivos com Shell
 
-Program TTY;
+program TTY;
+
 {$MODE OBJFPC}
 
-Uses crt, sysutils, strutils, Classes;
+//Bibliotecas utilizadas
+uses
+  crt,
+  SysUtils,
+  strutils,
+  Classes;
 
-Type 
-  TStringArray = array Of string;
-  TSysCharSet = set Of AnsiChar;
+//Criando tipos não primitivos
+type
+  TStringArray = array of string;
+  TSysCharSet = set of AnsiChar;
 
-Var 
-  entrada: String;
-  arg0 : String;
-  arg1 : String;
-  arg2 : String;
-  tam1 : integer;
-  tam2 : integer;
-  charset : TSysCharSet;
+//Variáveis utilizadas 
+var
+  entrada: string;
+  arg0: string;
+  arg1: string;
+  arg2: string;
+  tam1: integer;
+  tam2: integer;
+  charset: TSysCharSet;
   arq: TextFile;
-  outputString: String;
-  SR      : TSearchRec;
+  outputString: string;
+  SR: TSearchRec;
   texto: Text;
 
-Begin
+//Main do programa
+begin
   // teve que ser feito pois extractword não aceita char
   charset := [];
   include(charset, ' ');
 
   entrada := '';
 
-  While (CompareText(entrada,'exit')<>0) Do
-    Begin
-      write(GetCurrentDir);
-      write('>');
-      readLn(entrada);
+  //Enquanto não for digitado 'exit'
+  while (CompareText(entrada, 'exit') <> 0) do
+  begin
+    write(GetCurrentDir);
+    write('>');
+    readln(entrada);
 
-      arg0 := ExtractWord(1,entrada,charset);
-      arg1 := ExtractWord(2,entrada,charset);
-      arg2 := ExtractWord(3,entrada,charset);
-      tam1 := WordCount(arg1,charset);
-      tam2 := WordCount(arg2,charset);
+    arg0 := ExtractWord(1, entrada, charset);
+    arg1 := ExtractWord(2, entrada, charset);
+    arg2 := ExtractWord(3, entrada, charset);
+    tam1 := WordCount(arg1, charset);
+    tam2 := WordCount(arg2, charset);
 
-      Case arg0 Of 
-        'help' :
-                 Begin
-                   writeln('Comandos existentes:');
-                   writeln('man, exit, cat, ls, cd, mv, touch, locate, rmdir, mkdir, rm, clear');
-                 End;
-        'man' :
-                //terminar manuais para o arg1to dos comandos
-                Begin
-                  Case arg1 Of 
-                    'man' :
-                            Begin
-                              writeln('Mostra um manual sobre o comando fornecido');
-                            End;
-                    'exit' :
-                             Begin
-                               writeln('Fecha o terminal');
-                             End;
-                    'cat' :
-                            Begin
-                              writeln('Mostra o conteúdo de um arquivo passado por argumento');
-                            End;
-                    'ls' :
-                           Begin
-                             writeln('Mostra o conteúdo da pasta atual');
-                           End;
-                    'cd' :
-                           Begin
-                             writeln('Muda o diretório atual');
-                           End;
-                    'mv' :
-                           Begin
-                             writeln('Move ou renomeia um arquivo');
-                           End;
-                    'touch' :
-                              Begin
-                                writeln('Cria um arquivo');
-                              End;
-                    'locate' :
-                               Begin
-                                 writeln('Procura um arquivo na pasta atual');
-                               End;
-                    'rmdir' :
-                              Begin
-                                writeln('Remove um diretório da pasta autal');
-                              End;
-                    'mkdir' :
-                              Begin
-                                writeln('Cria um diretório na pasta atual');
-                              End;
-                    'rm' :
-                           Begin
-                             writeln('Remove um arquivo da pasta atual');
-                           End;
-                    'clear' :
-                              Begin
-                                writeln('Limpa a tela do terminal');
-                              End;
-                    'help' :
-                             Begin
-                               writeln('Mostra os comandos existentes');
-                             End;
-                    // caso padrão, comando vazio ou inexistente
-                    Else
-                      Begin
-                        If (tam1=0) Then
-                          Begin
-                            writeln('Qual manual gostaria de ver?');
-                          End;
-                        // if then else nao funciona (?????)
-                        If (tam1<>0) Then
-                          Begin
-                            writeln('Comando ' + arg1 + ' inexistente!');
-                          End;
-                      End;
-                  End;
-                End;
-        'exit' :
-                 Begin
-                   writeln('Bye!');
-                   exit;
-                 End;
-        '' : write();
-        'cat'  :
-                 Begin
-                   AssignFile(arq, arg1);
+    //Identificando o comando digitado pelo usuário
+    case arg0 of
+      //help: lista os comandos existentes
+      'help':
+      begin
+        writeln('Comandos existentes:');
+        writeln('man, exit, cat, ls, cd, mv, touch, locate, rmdir, mkdir, rm, clear');
+      end;
+      //man: mostra o manual do comando fornecido
+      'man': case arg1 of
+          'man': writeln('Mostra um manual sobre o comando fornecido');
+          'exit': writeln('Fecha o terminal');
+          'cat': writeln('Mostra o conteúdo de um arquivo passado por argumento');
+          'ls': writeln('Mostra o conteúdo da pasta atual');
+          'cd': writeln('Muda o diretório atual');
+          'mv': writeln('Move ou renomeia um arquivo');
+          'touch': writeln('Cria um arquivo');
+          'locate': writeln('Procura um arquivo na pasta atual');
+          'rmdir': writeln('Remove um diretório da pasta autal');
+          'mkdir': writeln('Cria um diretório na pasta atual');
+          'rm': writeln('Remove um arquivo da pasta atual');
+          'clear': writeln('Limpa a tela do terminal');
+          'help': writeln('Mostra os comandos existentes');
+            // caso padrão, comando vazio ou inexistente
+          else
+          begin
+            //Se comando não for fornecido
+            if (tam1 = 0) then
+              writeln('Qual manual gostaria de ver?');
+            // if then else nao funciona (?????)
+            //se comando não existe
+            if (tam1 <> 0) then
+              writeln('Comando ' + arg1 + ' inexistente!');
+          end;
+        end;
+      //exit: sai do terminal 
+      'exit':
+      begin
+        writeln('Bye!');
+        exit;
+      end;
+      //caso não seja digitado nada 
+      '': write();
+      //cat: Lê o conteúdo de um arquivo
+      'cat':
+      begin
+        AssignFile(arq, arg1);
                   {$I+}
-                   If (tam1<>0) Then
-                     Begin
-                       Try
-                         reset(arq);
-                         Repeat
-                           Readln(arq, outputString);
-                           Writeln(outputString);
-                         Until (EOF(arq));
-                         CloseFile(arq);
-                       Except
-                         on E: EInOutError Do writeln(E.Message);
-                     End;
-                 End;
-        If (tam1=0) Then
-          Begin
-            writeln('Digite um arquivo para ler!');
-          End;
-      End;
+        if (tam1 <> 0) then
+          //Inicia a leitura do arquivo                    
+          try
+            reset(arq);
+            repeat
+              readln(arq, outputString);
+              writeln(outputString);
+            until (EOF(arq));
+            CloseFile(arq);
+          except
+            on E: EInOutError do
+              writeln(E.Message);
+          end;
+        //Se não for passado nenhum arquivo 
+        if (tam1 = 0) then
+          writeln('Digite um arquivo para ler!');
+      end;
+      //ls: lista o conteúdo do diretório
       'ls':
-            Begin
-              If FindFirst ('*',faAnyFile And faDirectory,SR)=0 Then
-                Begin
-                  Repeat
-                    With SR Do
-                      Begin
-                        Write(SR.Name + ' ');
-                      End;
-                  Until FindNext(SR)<>0;
-                End;
-              writeln();
-              FindClose(SR);
-            End;
+        //Inicia a busca por um arquivo ou diretório
+      begin
+        if FindFirst('*', faAnyFile and faDirectory, SR) = 0 then
+          repeat
+            with SR do
+              write(SR.Name + ' ');
+          until FindNext(SR) <> 0;
+        writeln();
+        FindClose(SR);
+      end;
+      //cd: Entra dentro de um diretório
       'cd':
-            Begin
-              If (tam1<>0) Then
-                Begin
-                  Try
-                    ChDir (arg1);
-                  Except
-                    on E: EInOutError Do Writeln (E.Message);
-                End;
-            End;
-      If (tam1=0) Then ChDir('/home/'+GetEnvironmentVariable('USER'));
-
-    End;
-  'mv':
-        Begin
-          If (tam1<>0)Then
-            If (tam2<>0) Then
-              Begin
-                If Not(renamefile(arg1, arg2)) Then writeln('Erro ao mover arquivo!');
-              End;
-        End;
-  'touch':
-           Begin
-             If (tam1<>0) Then filecreate(arg1);
-             If (tam1=0) Then writeln('Digite o nome do arquivo para ser criado!');
-           End;
-  'locate':
-            Begin
-              If (tam1<>0) Then
-                Begin
-                  If FindFirst ('*'+arg1+'*',faAnyFile And faDirectory,SR)=0 Then
-                    Begin
-                      Repeat
-                        With SR Do
-                          Begin
-                            Write(SR.Name + ' ');
-                          End;
-                      Until FindNext(SR)<>0;
-                    End;
-                  writeln();
-                  FindClose(SR);
-                End;
-              If (tam1=0) Then writeln('Digite o nome do arquivo!');
-            End;
-  'rmdir':
-           Begin
-             Try
-               If (tam1 <> 0 ) Then rmdir(arg1);
-             Except
-               on E: EInOutError Do writeln(E.Message);
-           End;
-  If (tam1=0) Then writeln('Digite um diretório para excluir');
-End;
-'rm' :
-       Begin
-         Try
-           If (tam1 <> 0 ) Then
-             Begin
-               assign(texto,arg1);
-               erase(texto);
-             End;
-         Except
-           on E: EInOutError Do writeln(E.Message);
-       End;
-If (tam1=0) Then writeln('Digite um arquivo para excluir');
-
-End;
-'mkdir':
-         Begin
-           Try
-             If (tam1 <> 0 ) Then mkdir(arg1);
-           Except
-             on E: EInOutError Do writeln(E.Message);
-         End;
-If (tam1=0) Then writeln('Digite um diretório para criar');
-
-End;
-'clear' : clrscr();
-Else
-  writeln('Comando não reconhecido!');
-End;
-End;
-End.
+      begin
+        //muda para o diretório que foi fornecido pelo usuário
+        if (tam1 <> 0) then
+          try
+            ChDir(arg1);
+            //caso ocorra um erro
+          except
+            on E: EInOutError do
+              writeln(E.Message);
+          end;
+        //caso somente seja 'cd', muda para o /home/usuario
+        if (tam1 = 0) then
+          ChDir('/home/' + GetEnvironmentVariable('USER'));
+      end;
+      //mv: muda o nome do arquivo ou move o arquivo
+      'mv': if (tam1 <> 0) then
+          if (tam2 <> 0) then
+            if not (renamefile(arg1, arg2)) then
+              writeln('Erro ao mover arquivo!');
+      //touch: cria um novo arquivo
+      'touch':
+      begin
+        if (tam1 <> 0) then
+          filecreate(arg1);
+        //caso não seja fornecido um nome
+        if (tam1 = 0) then
+          writeln('Digite o nome do arquivo para ser criado!');
+      end;
+      //locate: localiza o arquivo dentro da pasta, todo: recursivo (diretórios)
+      'locate':
+      begin
+        //procura o arquivo com o nome fornecido 
+        if (tam1 <> 0) then
+        begin
+          if FindFirst('*' + arg1 + '*', faAnyFile and faDirectory, SR) = 0 then
+            repeat
+              with SR do
+                write(SR.Name + ' ');
+            until FindNext(SR) <> 0;
+          writeln();
+          FindClose(SR);
+        end;
+        //caso o nome do arquivo não for fornecido 
+        if (tam1 = 0) then
+          writeln('Digite o nome do arquivo!');
+      end;
+      //rmdir: exclui o diretório com o nome fornecido
+      'rmdir':
+      begin
+        //Tenta excluir o diretório
+        try
+          if (tam1 <> 0) then
+            rmdir(arg1);
+          //Caso ocorra algum erro
+        except
+          on E: EInOutError do
+            writeln(E.Message);
+        end;
+        //Caso o nome não seja fornecido
+        if (tam1 = 0) then
+          writeln('Digite um diretório para excluir');
+      end;
+      //rm: exclui o arquivo com o nome fornecido
+      'rm':
+      begin
+        //Tenta excluir o arquivo 
+        try
+          if (tam1 <> 0) then
+          begin
+            Assign(texto, arg1);
+            erase(texto);
+          end;
+          //Caso ocorra algum erro
+        except
+          on E: EInOutError do
+            writeln(E.Message);
+        end;
+        //Caso o nome do arquivo não seja fornecido
+        if (tam1 = 0) then
+          writeln('Digite um arquivo para excluir');
+      end;
+      //mkdir: cria um novo diretório
+      'mkdir':
+      begin
+        //tenta criar o novo diretório
+        try
+          if (tam1 <> 0) then
+            mkdir(arg1);
+          //caso ocorra algum erro
+        except
+          on E: EInOutError do
+            writeln(E.Message);
+        end;
+        //caso o nome do diretório não seja fornecido
+        if (tam1 = 0) then
+          writeln('Digite um diretório para criar');
+      end;
+      //clear: limpa a tela
+      'clear': clrscr();
+        //Caso padrão, não entrou em nenhum caso 
+      else writeln('Comando não reconhecido!');
+    end;
+  end;
+end.
